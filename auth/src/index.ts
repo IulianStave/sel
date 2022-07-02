2501c16565b58e0c4b5876c472da7c71ae91f1bc
Iulian Staverescu <iulian.staverescu@gmail.com>
2022-07-02 21:04:14 +0300

    Add ingress nginx controller

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -6,6 +6,10 @@ const app = express();
 
 app.use(json());
 
+app.get('/api/users/currentuser', (req, res) => {
+    res.send('Hi there from auth')
+});
+
 app.listen(3000, () => {
     console.log('Auth: Listening on port 3000');
 });
